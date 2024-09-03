76352001b7d5f1c0dbac7e2c80e900efa7153879
Bruno Parga <brunoparga@gmail.com>
2024-09-03 17:54:34 +0300

    Go a level deeper in the transactions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 use zephyr_sdk::{
-    soroban_sdk::xdr::{OperationResult, OperationResultCode, TransactionResultResult::TxSuccess},
+    soroban_sdk::xdr::{
+        OperationResult, OperationResultTr, TransactionResult, TransactionResultMeta,
+        TransactionResultPair,
+        TransactionResultResult::{self, TxSuccess},
+        VecM,
+    },
     EnvClient,
 };
 
@@ -8,44 +13,52 @@ pub extern "C" fn on_close() {
     let env = EnvClient::new();
     let reader = env.reader();
     let sequence = reader.ledger_sequence();
-    // let events = reader.tx_processing();
+    let events: Vec<TransactionResultMeta> = reader.tx_processing();
+
+    let successful_operations: Vec<OperationResultTr> = events
+        .into_iter()
+        .filter_map(event_successful_operations)
+        .flatten()
+        .collect::<Vec<OperationResultTr>>();
 
     env.log().debug(
-        format!("Sequence {}", sequence),
-        // &format!("Sequence {} has {} events", sequence, events.len()),
+        &format!(
+            "Sequence {} has {} payment operations",
+            sequence,
+            successful_operations.len()
+        ),
         None,
     );
 
-    // let successful_operations: Vec<OperationResult> = events
-    //     .into_iter()
-    //     .filter_map(|event| {
-    //         let result_pair = event.result;
-    //         let transaction_result = result_pair.result;
-    //         let transaction_result_result_yes_really: zephyr_sdk::soroban_sdk::xdr::TransactionResultResult = transaction_result.result;
+    let formatted = format!("{:?}", successful_operations.first());
+    // let message = formatted.chars().take(1024).collect::<String>() + "...";
 
-    //         if let TxSuccess(op_results) = transaction_result_result_yes_really {
-    //             Some(
-    //                 op_results
-    //                     .iter()
-    //                     .filter(|op_result| op_result.discriminant() == OperationResultCode::OpInner)
-    //                     .cloned()
-    //                     .collect::<Vec<_>>())
-    //         } else {
-    //             None
-    //         }
-    //     })
-    //     .flatten()
-    //     .collect();
+    env.log().debug(formatted, None)
+}
 
-    // env.log().debug(
-    //     &format!(
-    //         "On sequence {} there are {} successful operations",
-    //         sequence,
-    //         successful_operations.len()
-    //     ),
-    //     None,
-    // );
+fn event_successful_operations(event: TransactionResultMeta) -> Option<Vec<OperationResultTr>> {
+    let result_pair: TransactionResultPair = event.result;
+    let transaction_result: TransactionResult = result_pair.result;
+    let transaction_result_result: TransactionResultResult = transaction_result.result;
 
-    // env.log()
-    //     .debug(&format!("{:.280?}...", successful_operations), None)
+    if let TxSuccess(op_results) = transaction_result_result {
+        Some(transaction_operations(op_results))
+    } else {
+        None
+    }
+}
+
+fn transaction_operations(op_results: VecM<OperationResult, 4294967295>) -> Vec<OperationResultTr> {
+    op_results
+        .iter()
+        .filter_map(|outer: &OperationResult| match outer {
+            OperationResult::OpInner(inner) => match inner {
+                OperationResultTr::Payment(_)
+                | OperationResultTr::PathPaymentStrictReceive(_)
+                | OperationResultTr::PathPaymentStrictSend(_) => Some(inner.clone()),
+                _ => None,
+            },
+            _ => None,
+        })
+        .collect::<Vec<OperationResultTr>>()
 }
