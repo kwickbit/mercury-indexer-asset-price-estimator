3a66d88e154ed37ec3b9e5db0359039e213680b5
Bruno Parga <brunoparga@gmail.com>
2024-09-05 12:39:45 +0300

    Confirm that I am indeed filtering only payments

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 use zephyr_sdk::{
     soroban_sdk::xdr::{
-        OperationResult, OperationResultTr, TransactionResult, TransactionResultMeta,
-        TransactionResultPair,
-        TransactionResultResult::{self, TxSuccess},
-        VecM,
+        OperationResult, OperationResultTr, TransactionResultResult::TxSuccess, VecM,
     },
     EnvClient,
 };
@@ -13,42 +10,47 @@ pub extern "C" fn on_close() {
     let env = EnvClient::new();
     let reader = env.reader();
     let sequence = reader.ledger_sequence();
-    let events: Vec<TransactionResultMeta> = reader.tx_processing();
+    let events = reader.tx_processing();
 
-    let successful_operations: Vec<OperationResultTr> = events
+    let successful_ops: Vec<OperationResult> = events
+        .clone()
         .into_iter()
-        .filter_map(event_successful_operations)
+        .filter_map(|event| {
+            if let TxSuccess(op_results) = event.result.result.result {
+                Some(op_results.into_vec())
+            } else {
+                None
+            }
+        })
         .flatten()
-        .collect::<Vec<OperationResultTr>>();
+        .collect();
+
+    let payments: Vec<OperationResultTr> = events
+        .into_iter()
+        .filter_map(|event| {
+            if let TxSuccess(op_results) = event.result.result.result {
+                Some(transaction_payment_operations(op_results))
+            } else {
+                None
+            }
+        })
+        .flatten()
+        .collect();
 
     env.log().debug(
         &format!(
-            "Sequence {} has {} payment operations",
+            "Sequence {} has {} operations, of which {} are payments",
             sequence,
-            successful_operations.len()
+            successful_ops.len(),
+            payments.len()
         ),
         None,
     );
-
-    let formatted = format!("{:?}", successful_operations.first());
-    // let message = formatted.chars().take(1024).collect::<String>() + "...";
-
-    env.log().debug(formatted, None)
 }
 
-fn event_successful_operations(event: TransactionResultMeta) -> Option<Vec<OperationResultTr>> {
-    let result_pair: TransactionResultPair = event.result;
-    let transaction_result: TransactionResult = result_pair.result;
-    let transaction_result_result: TransactionResultResult = transaction_result.result;
-
-    if let TxSuccess(op_results) = transaction_result_result {
-        Some(transaction_operations(op_results))
-    } else {
-        None
-    }
-}
-
-fn transaction_operations(op_results: VecM<OperationResult, 4294967295>) -> Vec<OperationResultTr> {
+fn transaction_payment_operations(
+    op_results: VecM<OperationResult, 4294967295>,
+) -> Vec<OperationResultTr> {
     op_results
         .iter()
         .filter_map(|outer: &OperationResult| match outer {
