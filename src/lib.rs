bfa86c80c57d48517bd8deebe1bc942d5f3a7ce4
Bruno Parga <brunoparga@gmail.com>
2024-09-06 12:32:28 +0300

    Refactor and include milestones for empty closes

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,87 @@
 use zephyr_sdk::{
     soroban_sdk::xdr::{
-        OperationResult, OperationResultTr, TransactionResultResult::TxSuccess, VecM,
+        AlphaNum4, Asset, OperationBody, PathPaymentStrictReceiveOp, PathPaymentStrictSendOp,
+        PaymentOp, TransactionEnvelope, TransactionResultMeta, TransactionResultResult,
     },
     EnvClient,
 };
+
+const ASSET: &str = "USDC";
 
 #[no_mangle]
 pub extern "C" fn on_close() {
     let env = EnvClient::new();
     let reader = env.reader();
     let sequence = reader.ledger_sequence();
-    let events = reader.tx_processing();
+    let events = reader.envelopes_with_meta();
+    let successful = successful_usdc_txns(&events);
 
-    let successful_ops: Vec<OperationResult> = events
-        .clone()
-        .into_iter()
-        .filter_map(|event| {
-            if let TxSuccess(op_results) = event.result.result.result {
-                Some(op_results.into_vec())
-            } else {
-                None
-            }
-        })
-        .flatten()
-        .collect();
+    if successful.is_empty() && sequence % 12 == 0 {
+        env.log().debug(&format!("Sequence {}", sequence), None);
+    }
 
-    let payments: Vec<OperationResultTr> = events
-        .into_iter()
-        .filter_map(|event| {
-            if let TxSuccess(op_results) = event.result.result.result {
-                Some(transaction_payment_operations(op_results))
-            } else {
-                None
-            }
-        })
-        .flatten()
-        .collect();
-
-    env.log().debug(
-        &format!(
-            "Sequence {} has {} operations, of which {} are payments",
-            sequence,
-            successful_ops.len(),
-            payments.len()
-        ),
-        None,
-    );
+    if !successful.is_empty() {
+        env.log().debug(
+            &format!(
+                "Sequence {} has {} successful {} transactions",
+                sequence,
+                successful.len(),
+                ASSET,
+            ),
+            None,
+        );
+    }
 }
 
-fn transaction_payment_operations(
-    op_results: VecM<OperationResult, 4294967295>,
-) -> Vec<OperationResultTr> {
-    op_results
-        .iter()
-        .filter_map(|outer: &OperationResult| match outer {
-            OperationResult::OpInner(inner) => match inner {
-                OperationResultTr::Payment(_)
-                | OperationResultTr::PathPaymentStrictReceive(_)
-                | OperationResultTr::PathPaymentStrictSend(_) => Some(inner.clone()),
-                _ => None,
-            },
-            _ => None,
-        })
-        .collect::<Vec<OperationResultTr>>()
+fn successful_usdc_txns<'a>(
+    events: &[(&'a TransactionEnvelope, &TransactionResultMeta)],
+) -> Vec<&'a TransactionEnvelope> {
+    events.iter().filter_map(is_successful_usdc_txn).collect()
+}
+
+fn is_successful_usdc_txn<'a>(
+    (envelope, result_meta): &(&'a TransactionEnvelope, &TransactionResultMeta),
+) -> Option<&'a TransactionEnvelope> {
+    if is_usdc(envelope) && is_successful(result_meta) {
+        Some(envelope)
+    } else {
+        None
+    }
+}
+
+fn is_usdc(envelope: &TransactionEnvelope) -> bool {
+    let operations = match envelope {
+        TransactionEnvelope::TxV0(v0env) => &v0env.tx.operations,
+        TransactionEnvelope::Tx(v0env) => &v0env.tx.operations,
+        _ => return false,
+    };
+
+    operations.iter().any(|op| match &op.body {
+        OperationBody::Payment(PaymentOp {
+            asset: Asset::CreditAlphanum4(asset),
+            ..
+        }) => asset_matches(asset, ASSET),
+        OperationBody::PathPaymentStrictReceive(PathPaymentStrictReceiveOp {
+            send_asset: Asset::CreditAlphanum4(send_asset),
+            dest_asset: Asset::CreditAlphanum4(dest_asset),
+            ..
+        }) => asset_matches(send_asset, ASSET) || asset_matches(dest_asset, ASSET),
+        OperationBody::PathPaymentStrictSend(PathPaymentStrictSendOp {
+            send_asset: Asset::CreditAlphanum4(send_asset),
+            dest_asset: Asset::CreditAlphanum4(dest_asset),
+            ..
+        }) => asset_matches(send_asset, ASSET) || asset_matches(dest_asset, ASSET),
+        _ => false,
+    })
+}
+
+fn is_successful(result_meta: &TransactionResultMeta) -> bool {
+    matches!(
+        result_meta.result.result.result,
+        TransactionResultResult::TxSuccess(_)
+    )
+}
+
+fn asset_matches(asset: &AlphaNum4, code: &str) -> bool {
+    asset.asset_code.as_slice() == code.as_bytes()
 }
