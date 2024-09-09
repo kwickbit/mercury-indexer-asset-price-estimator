38f9d66885444b820459e038c3498efbaf4ea6ca
Bruno Parga <brunoparga@gmail.com>
2024-09-09 18:47:28 +0300

    Implement a custom type

--- src/utils.rs
+++ src/utils.rs
@@ -2,10 +2,17 @@ use zephyr_sdk::soroban_sdk::xdr::{Operation, TransactionEnvelope, VecM};
 
 pub const ASSET: &str = "USDC";
 
-pub fn transaction_operations(transaction: &TransactionEnvelope) -> VecM<Operation, 100> {
+pub fn extract_transaction_operations(transaction: &TransactionEnvelope) -> VecM<Operation, 100> {
     match transaction {
         TransactionEnvelope::TxV0(envelope) => envelope.tx.operations.clone(),
         TransactionEnvelope::Tx(envelope) => envelope.tx.operations.clone(),
         _ => Default::default(),
+    }
+}
+
+pub fn bytes_to_string(bytes: &[u8]) -> String {
+    match std::str::from_utf8(bytes) {
+        Ok(alpha) => alpha.to_string(),
+        Err(err) => err.to_string(),
     }
 }
