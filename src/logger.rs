69a7e42f93338dcbc67323490e4324da6a6159b7
Bruno Parga <brunoparga@gmail.com>
2024-09-09 16:27:42 +0300

    Add num-format crate

--- src/logger.rs
+++ src/logger.rs
@@ -1,4 +1,12 @@
-use zephyr_sdk::{soroban_sdk::xdr::{AlphaNum12, AlphaNum4, Asset, OperationBody, PathPaymentStrictReceiveOp, PathPaymentStrictSendOp, PaymentOp, TransactionEnvelope, VecM}, EnvLogger};
+use num_format::{Locale, ToFormattedString};
+
+use zephyr_sdk::{
+    soroban_sdk::xdr::{
+        AlphaNum12, AlphaNum4, Asset, OperationBody, PathPaymentStrictReceiveOp,
+        PathPaymentStrictSendOp, TransactionEnvelope, TransactionResultMeta, VecM,
+    },
+    EnvLogger,
+};
 
 use crate::utils;
 
@@ -59,7 +67,13 @@ fn fmt_op_in_usdc_txn(operation: &OperationBody) -> String {
     }
 }
 
-fn format_path(path: &VecM<Asset, 5>) -> String {
+fn format_amount(amount: &i64) -> String {
+    let conversion_factor = 10_000_000;
+    let integer_amount = *amount / conversion_factor;
+    let formatted_integer = integer_amount.to_formatted_string(&Locale::en);
+    let fractional_amount = (*amount % conversion_factor) as f64;
+    format!("{}.{}", formatted_integer, fractional_amount)
+}
     path.iter()
         .map(format_asset)
         .collect::<Vec<&str>>()
