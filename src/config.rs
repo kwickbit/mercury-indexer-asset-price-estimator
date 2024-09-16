a01e886b599538cfefceb16fc08dce32f07772ff
Bruno Parga <brunoparga@gmail.com>
2024-09-16 16:07:16 +0300

    Document the config

--- src/config.rs
+++ src/config.rs
@@ -1,2 +1,6 @@
-pub const FORCE_MILESTONE: bool = true;
+// Do I want milestones to be printed even when there are transactions?
+pub const FORCE_MILESTONE: bool = false;
+
+// How often should milestones be printed?
+// There are 11-12 sequences every minute.
 pub const MILESTONE_INTERVAL: u32 = 12;
