3636d9dfb1b69ab5ea1a0b53da99dfe7acb92749
Bruno Parga <brunoparga@gmail.com>
2024-09-16 18:40:03 +0300

    Add database interaction

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,13 @@
 // Do I want milestones to be printed even when there are transactions?
-pub const FORCE_MILESTONE: bool = false;
+pub const FORCE_MILESTONE: bool = true;
 
 // How often should milestones be printed?
 // There are 11-12 sequences every minute.
 pub const MILESTONE_INTERVAL: u32 = 12;
+
+// Whether to print details of interesting transactions
+// We always print at least the number of interesting transactions.
+pub const PRINT_TRANSACTION_DETAILS: bool = true;
+
+// Whether to interact with the DB at all
+pub const DO_DB_STUFF: bool = false;
