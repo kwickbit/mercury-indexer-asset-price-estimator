980120dcc289529fb4479bcd9ad6063e1a88564b
Bruno Parga <brunoparga@gmail.com>
2024-09-16 15:38:05 +0300

    Remove unused module exchange_rate

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,9 +1,7 @@
 use zephyr_sdk::soroban_sdk::xdr::{
     Operation, OperationResult, TransactionEnvelope, TransactionResultMeta, TransactionResultResult,
 };
-use zephyr_sdk::EnvClient;
 
-use crate::exchange_rate::{extract_exchange_rates, ExchangeRate};
 use crate::utils;
 
 #[derive(Debug)]
@@ -25,21 +23,5 @@ impl InterestingTransaction {
             operations: operations.to_vec(),
             results,
         }
-    }
-
-    #[allow(dead_code)]
-    pub fn exchange_rates(&self, client: &EnvClient) -> Vec<ExchangeRate> {
-        client
-            .log()
-            .debug(&format!("Operations: {:#?}", self.operations), None);
-        client
-            .log()
-            .debug(&format!("Results: {:#?}", self.results), None);
-        self.operations
-            .iter()
-            .zip(self.results.iter())
-            .filter_map(|(op, result)| extract_exchange_rates(op, result))
-            .flatten()
-            .collect()
     }
 }
