d7a5373156fc25481164b9feb9560befdacce38c
Bruno Parga <brunoparga@gmail.com>
2024-09-23 11:48:28 +0300

    Save swaps to the DB

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
 mod config;
-// mod db;
+mod db;
 mod filter;
-// mod log;
 mod swap;
 mod utils;
 
-use config::DO_DB_STUFF;
 use zephyr_sdk::{EnvClient, EnvLogger};
-// use db::do_db_stuff;
-// use log::log;
+use db::save_swaps;
 
 #[no_mangle]
 pub extern "C" fn on_close() {
@@ -23,11 +20,12 @@ pub extern "C" fn on_close() {
     let env_logger = client.log();
     let logger = create_logger(&env_logger);
     let swaps = filter::swaps(reader.tx_processing());
-    logger(&format!("In sequence {sequence}, processed {} swaps", swaps.len()));
+    logger(&format!(
+        "In sequence {sequence}, processed {} swaps",
+        swaps.len()
+    ));
 
-    if DO_DB_STUFF {
-        // do_db_stuff(client, swaps);
-    }
+    save_swaps(client, swaps);
 }
 
 fn create_logger(env: &EnvLogger) -> impl Fn(&str) + '_ {
