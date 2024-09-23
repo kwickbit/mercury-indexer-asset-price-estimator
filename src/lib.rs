0a11ddb722b365c37472a759257a5c841c602b46
Bruno Parga <brunoparga@gmail.com>
2024-09-23 19:03:07 +0300

    Make the code a bit prettier

--- src/lib.rs
+++ src/lib.rs
@@ -8,28 +8,29 @@ mod utils;
 // but are here because I will soon use.
 use db::SwapDbRow;
 use swap::Swap;
-use zephyr_sdk::{EnvClient, EnvLogger};
+use zephyr_sdk::EnvClient;
 
 #[no_mangle]
 pub extern "C" fn on_close() {
     // The Zephyr client
     let client = EnvClient::new();
+    let logger = create_logger(&client);
 
     let rows = client.read::<SwapDbRow>();
     let first_row: Swap = rows.first().unwrap().into();
 
-    client.log().debug(
-        &format!("Found {} swaps. First one: {first_row}", rows.len()),
-        None,
-    );
+    logger(&format!(
+        "Found {} swaps. First one: {first_row}",
+        rows.len()
+    ));
 
     if config::SAVE_SWAPS_TO_DATABASE {
         let swaps = filter::swaps(client.reader().tx_processing());
         db::save_swaps(&client, &swaps);
-        create_logger(&client.log())(&format!("Saved {} swaps to the database", swaps.len()));
+        logger(&format!("Saved {} swaps to the database", swaps.len()));
     }
 }
 
-fn create_logger(env: &EnvLogger) -> impl Fn(&str) + '_ {
-    move |args| env.debug(args, None)
+fn create_logger(env: &EnvClient) -> impl Fn(&str) + '_ {
+    move |args| env.log().debug(args, None)
 }
