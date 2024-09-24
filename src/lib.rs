06b0a79f25464f83e763eab1357e22ad84fedff7
Bruno Parga <brunoparga@gmail.com>
2024-09-24 12:06:25 +0300

    Not log the rates every sequence

--- src/lib.rs
+++ src/lib.rs
@@ -5,30 +5,35 @@ mod filter;
 mod swap;
 mod utils;
 
-// Note to self: qualified imports mean things I am not currently using,
-// but are here because I will soon use.
+use config::{LOGGING_INTERVAL, SAVE_SWAPS_TO_DATABASE};
+use db::save_swaps;
 use zephyr_sdk::EnvClient;
 
 #[no_mangle]
 pub extern "C" fn on_close() {
-    // The Zephyr client
     let client = EnvClient::new();
+    let sequence = client.reader().ledger_sequence();
     let logger = create_logger(&client);
-    let exchange_rates = exchange_rate::calculate_exchange_rates(&client);
 
-    let log_message = exchange_rates
-        .iter()
-        .map(|(coin, (weighted_average, volume))| {
-            format!("{coin}: {weighted_average:.4} (volume ${volume:.2})")
-        })
-        .collect::<Vec<_>>()
-        .join(", ");
+    if sequence % LOGGING_INTERVAL == 0 {
+        let exchange_rates = exchange_rate::calculate_exchange_rates(&client);
 
-    logger(&format!("Exchange rates by floatcoin: {}", log_message));
+        let log_message = exchange_rates
+            .iter()
+            .map(|(coin, (weighted_average, volume))| {
+                format!("{coin}: {weighted_average:.4} (volume ${volume:.2})")
+            })
+            .collect::<Vec<_>>()
+            .join(", ");
 
-    if config::SAVE_SWAPS_TO_DATABASE {
+        logger(&format!(
+            "Sequence {sequence}, exchange rates: {log_message}"
+        ));
+    }
+
+    if SAVE_SWAPS_TO_DATABASE {
         let swaps = filter::swaps(client.reader().tx_processing());
-        db::save_swaps(&client, &swaps);
+        save_swaps(&client, &swaps);
         logger(&format!("Saved {} swaps to the database", swaps.len()));
     }
 }
