0cb54be9473af5fd11a26130e4bed87ba9966b63
Bruno Parga <brunoparga@gmail.com>
2024-09-24 10:58:36 +0300

    Log the exchange rates and volumes

--- src/exchange_rate.rs
+++ src/exchange_rate.rs
@@ -1,20 +1,53 @@
+use std::collections::HashMap;
+
 use zephyr_sdk::EnvClient;
 
-use crate::db::SwapDbRow;
+use crate::{config::CONVERSION_FACTOR, db::SwapDbRow};
+
+pub type UsdVolume = f64;
+pub type WeightedSum = f64;
+pub type ExchangeRate = f64;
 
 pub fn calculate_exchange_rates(client: &EnvClient) -> String {
     let rows = client.read::<SwapDbRow>();
 
-    let floatcoin_counts = rows
+    let exchange_rates: HashMap<&String, (ExchangeRate, UsdVolume)> = rows
         .iter()
-        .fold(std::collections::HashMap::new(), |mut counts, row| {
-            *counts.entry(&row.floating).or_insert(0) += 1;
-            counts
-        });
+        .fold(HashMap::new(), extract_rates)
+        // Now that we have the total swapped amounts, we calculate the exchange rate
+        .into_iter()
+        .map(|(key, (weighted_sum, total_amount))| {
+            (key, (weighted_sum / total_amount, total_amount))
+        })
+        .collect();
 
-    floatcoin_counts
+    exchange_rates
         .iter()
-        .map(|(coin, count)| format!("{} {}", count, coin))
+        .map(|(coin, (weighted_average, volume))| {
+            format!("{coin}: {weighted_average:.6} (volume ${volume:.6})")
+        })
         .collect::<Vec<_>>()
         .join(", ")
+}
+
+fn extract_rates<'a>(
+    mut counts: HashMap<&'a String, (WeightedSum, UsdVolume)>,
+    row: &'a SwapDbRow,
+) -> HashMap<&'a String, (WeightedSum, UsdVolume)> {
+    let amount: UsdVolume = row.stableamt as f64 / CONVERSION_FACTOR;
+    let rate: WeightedSum = row.numerator as f64 / row.denom as f64;
+
+    // For XLM swaps, we sometimes get weird values, so we don't include them
+    if rate != 1e-7 {
+        // Update the entry with a running sum of (weighted_sum, total_amount)
+        counts
+            .entry(&row.floating)
+            .and_modify(|(weighted_sum, total_amount)| {
+                *weighted_sum += amount * rate;
+                *total_amount += amount;
+            })
+            .or_insert((amount * rate, amount));
+    }
+
+    counts
 }
