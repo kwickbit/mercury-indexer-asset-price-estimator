ec6885b5f21ec303c7ce82a9d1b865434f80f1c4
Bruno Parga <brunoparga@gmail.com>
2024-09-24 17:43:56 +0300

    Implement a function to return the exchange rate

--- src/config.rs
+++ src/config.rs
@@ -12,7 +12,7 @@ so it made sense to group them here.
 
 // How often do we log exchange rates, measured in sequences
 // There are 11-12 sequences per minute
-pub const LOGGING_INTERVAL: u32 = 48;
+pub const LOGGING_INTERVAL: u32 = 4800;
 
 // Do we save the swaps to the database?
-pub const SAVE_SWAPS_TO_DATABASE: bool = true;
+pub const SAVE_SWAPS_TO_DATABASE: bool = false;
