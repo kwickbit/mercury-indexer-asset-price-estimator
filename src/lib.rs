ba348910622366530414eeda8b6fc13e0142781f
Bruno Parga <brunoparga@gmail.com>
2024-09-24 17:43:56 +0300

    Implement a function to return the exchange rate

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ mod utils;
 
 use config::{LOGGING_INTERVAL, SAVE_SWAPS_TO_DATABASE};
 use db::save_swaps;
+use serde::{Deserialize, Serialize};
 use zephyr_sdk::EnvClient;
 
 #[no_mangle]
@@ -40,4 +41,32 @@ pub extern "C" fn on_close() {
 
 fn create_logger(env: &EnvClient) -> impl Fn(&str) + '_ {
     move |args| env.log().debug(args, None)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Request {
+    asset: String,
+}
+
+#[no_mangle]
+pub extern "C" fn get_exchange_rate() {
+    let client = EnvClient::empty();
+    let request: Request = client.read_request_body();
+    let exchange_rates = exchange_rate::calculate_exchange_rates(&client);
+    match exchange_rates.get(&request.asset) {
+        Some(&(rate, _)) => {
+            let response = serde_json::json!({
+                "asset": request.asset,
+                "exchange_rate": rate.to_string(),
+            });
+            client.conclude(serde_json::to_string(&response).unwrap());
+        }
+        None => {
+            let response = serde_json::json!({
+                "asset": request.asset,
+                "error": "Asset not found",
+            });
+            client.conclude(serde_json::to_string(&response).unwrap());
+        }
+    }
 }
