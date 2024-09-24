a21ffc855c0956b1ca708a08881071ca70e884ce
Bruno Parga <brunoparga@gmail.com>
2024-09-24 09:53:36 +0300

    Split exchange rate processing into its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 mod config;
 mod db;
+mod exchange_rate;
 mod filter;
 mod swap;
 mod utils;
 
 // Note to self: qualified imports mean things I am not currently using,
 // but are here because I will soon use.
-use db::SwapDbRow;
 use zephyr_sdk::EnvClient;
 
 #[no_mangle]
@@ -14,22 +14,9 @@ pub extern "C" fn on_close() {
     // The Zephyr client
     let client = EnvClient::new();
     let logger = create_logger(&client);
-    let rows = client.read::<SwapDbRow>();
+    let exchange_rates = exchange_rate::calculate_exchange_rates(&client);
 
-    let floatcoin_counts = rows
-        .iter()
-        .fold(std::collections::HashMap::new(), |mut counts, row| {
-            *counts.entry(&row.floating).or_insert(0) += 1;
-            counts
-        });
-
-    let log_message = floatcoin_counts
-        .iter()
-        .map(|(coin, count)| format!("{} {}", count, coin))
-        .collect::<Vec<_>>()
-        .join(", ");
-
-    logger(&format!("Found {} swaps. Counts by floatcoin: {}", rows.len(), log_message));
+    logger(&format!("Swap counts by floatcoin: {}", exchange_rates));
 
     if config::SAVE_SWAPS_TO_DATABASE {
         let swaps = filter::swaps(client.reader().tx_processing());
