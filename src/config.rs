5f49c6225435eb7c6bcb287c6f17f2038364e03a
Bruno Parga <brunoparga@gmail.com>
2024-09-25 17:21:45 +0300

    Set a savepoint that's updated every 5 minutes

--- src/config.rs
+++ src/config.rs
@@ -19,3 +19,6 @@ pub const SAVE_SWAPS_TO_DATABASE: bool = true;
 
 // Do we save the exchange rates to the database?
 pub const SAVE_RATES_TO_DATABASE: bool = false;
+
+// How often do we update the savepoint in seconds?
+pub const SAVEPOINT_UPDATE_INTERVAL: u64 = 300;
