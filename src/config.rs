273801bc01e5f6e97441f00f699bdbd256bb374e
Bruno Parga <brunoparga@gmail.com>
2024-09-26 17:47:42 +0300

    Only use swaps from window to calculate rates

--- src/config.rs
+++ src/config.rs
@@ -4,21 +4,5 @@ pub const STABLECOINS: [&str; 3] = ["USDC", "USDT", "DAI"];
 // Amounts are represented with this factor
 pub const CONVERSION_FACTOR: f64 = 10_000_000.0;
 
-/*
-The following constants are mostly for convenience.
-These things were changing very quickly during early development,
-so it made sense to group them here.
-*/
-
-// How often do we log exchange rates, measured in sequences
-// There are 11-12 sequences per minute
-pub const LOGGING_INTERVAL: u32 = 25;
-
-// Do we save the swaps to the database?
-pub const SAVE_SWAPS_TO_DATABASE: bool = true;
-
-// Do we save the exchange rates to the database?
-pub const SAVE_RATES_TO_DATABASE: bool = true;
-
-// How often do we update the savepoint in seconds?
-pub const RATE_UPDATE_INTERVAL: u64 = 300;
+// Length of the exchange rate window in seconds
+pub const RATE_UPDATE_INTERVAL: u64 = 900;
--- src/db.rs
+++ src/db.rs
@@ -4,7 +4,7 @@ pub(crate) mod swap;
 
 use zephyr_sdk::{DatabaseInteract, EnvClient};
 
-use exchange_rate::{ExchangeRateMap, RatesDbRow};
+use exchange_rate::RatesDbRow;
 use savepoint::Savepoint;
 use swap::{Swap, SwapDbRow};
 
@@ -17,18 +17,79 @@ pub fn save_swaps(client: &EnvClient, swaps: &[Swap]) {
         .for_each(|swap| SwapDbRow::new(swap, timestamp).put(client));
 }
 
-pub fn save_rates(client: &EnvClient, rates: &ExchangeRateMap) {
-    // This panics right when we first deploy and there is no savepoint.
-    // That is acceptable.
-    let latest_savepoint = client.read::<Savepoint>()[0].savepoint;
-    let current_timestamp = client.reader().ledger_timestamp();
-    let should_save_rates = current_timestamp - latest_savepoint > RATE_UPDATE_INTERVAL;
+pub fn save_rates(client: &EnvClient) {
+    let savepoints = client.read::<Savepoint>();
 
-    if should_save_rates {
+    // We only create a savepoint if none is found, and update
+    // otherwise. This means we should never have more than one.
+    if savepoints.len() > 1 {
+        client
+            .log()
+            .error("Error: more than one savepoint found.", None);
+        return;
+    }
+
+    let current_timestamp = client.reader().ledger_timestamp();
+    let mut latest_savepoint = current_timestamp - RATE_UPDATE_INTERVAL;
+
+    // We check if it is time to save the exchange rates.
+    let is_time_to_save_rates = if savepoints.is_empty() {
+        // When we force-deploy there is no savepoint, so we create one.
+        first_savepoint(client, current_timestamp);
+
+        // In that case we always save the rates.
+        true
+    } else {
+        // If there is a savepoint, we only save fresh rates if
+        // the existing ones are stale.
+        latest_savepoint = savepoints[0].savepoint;
+        let are_rates_stale = current_timestamp - latest_savepoint > RATE_UPDATE_INTERVAL;
+
+        if are_rates_stale {
+            update_savepoint(client, current_timestamp, latest_savepoint);
+        }
+
+        are_rates_stale
+    };
+
+    if is_time_to_save_rates {
+        let rates = exchange_rate::calculate_exchange_rates(client, latest_savepoint);
+
         rates.iter().for_each(|item| {
             let mut row = RatesDbRow::from(item);
             row.timestamp = Some(current_timestamp);
             row.put(client);
         });
     }
+}
+
+fn first_savepoint(client: &EnvClient, current_timestamp: u64) {
+    let savepoint = Savepoint {
+        savepoint: current_timestamp,
+    };
+
+    savepoint.put(client);
+    client
+        .log()
+        .debug(&format!("First savepoint: {current_timestamp}"), None);
+}
+
+fn update_savepoint(client: &EnvClient, current_timestamp: u64, latest_savepoint: u64) {
+    let savepoint = Savepoint {
+        savepoint: current_timestamp,
+    };
+
+    if let Err(sdk_error) = client
+        .update()
+        .column_equal_to("savepoint", latest_savepoint)
+        .execute(&savepoint)
+    {
+        client.log().debug(
+            &format!(
+                "Sequence {} failed to update savepoint: {sdk_error}",
+                client.reader().ledger_sequence()
+            ),
+            None,
+        )
+    };
 }
