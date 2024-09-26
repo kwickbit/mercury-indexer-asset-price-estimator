da6fcb4c4eed09099c888089ae2f2989dacf468e
Bruno Parga <brunoparga@gmail.com>
2024-09-26 15:26:22 +0300

    Save exchange rates every given interval

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -5,7 +5,10 @@ pub(crate) mod swap;
 use zephyr_sdk::{DatabaseInteract, EnvClient};
 
 use exchange_rate::{ExchangeRateMap, RatesDbRow};
+use savepoint::Savepoint;
 use swap::{Swap, SwapDbRow};
+
+use crate::config::RATE_UPDATE_INTERVAL;
 
 pub fn save_swaps(client: &EnvClient, swaps: &[Swap]) {
     let timestamp = client.reader().ledger_timestamp();
@@ -14,17 +17,18 @@ pub fn save_swaps(client: &EnvClient, swaps: &[Swap]) {
         .for_each(|swap| SwapDbRow::new(swap, timestamp).put(client));
 }
 
-pub fn save_rates(client: &EnvClient, rates: &ExchangeRateMap) -> bool {
-    let should_save_rates = client.read::<RatesDbRow>().is_empty();
+pub fn save_rates(client: &EnvClient, rates: &ExchangeRateMap) {
+    // This panics right when we first deploy and there is no savepoint.
+    // That is acceptable.
+    let latest_savepoint = client.read::<Savepoint>()[0].savepoint;
+    let current_timestamp = client.reader().ledger_timestamp();
+    let should_save_rates = current_timestamp - latest_savepoint > RATE_UPDATE_INTERVAL;
 
     if should_save_rates {
-        let ledger_timestamp = client.reader().ledger_timestamp();
         rates.iter().for_each(|item| {
             let mut row = RatesDbRow::from(item);
-            row.timestamp = Some(ledger_timestamp);
+            row.timestamp = Some(current_timestamp);
             row.put(client);
         });
     }
-
-    should_save_rates
 }
