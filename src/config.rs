fb3d2379d6fe0e3c4665488ead16953a61d05b32
Bruno Parga <brunoparga@gmail.com>
2024-09-30 11:04:24 +0300

    Make window lengths more explicit

--- src/config.rs
+++ src/config.rs
@@ -4,5 +4,8 @@ pub const STABLECOINS: [&str; 3] = ["USDC", "USDT", "DAI"];
 // Amounts are represented with this factor
 pub const CONVERSION_FACTOR: f64 = 10_000_000.0;
 
-// Length of the exchange rate window in seconds
-pub const RATE_UPDATE_INTERVAL: u64 = 900;
+// Length of the exchange rate window
+const MINUTE: u64 = 60;
+const _HOUR: u64 = 60 * MINUTE;
+const _DAY: u64 = 24 * _HOUR;
+pub const RATE_UPDATE_INTERVAL: u64 = 15 * MINUTE;
