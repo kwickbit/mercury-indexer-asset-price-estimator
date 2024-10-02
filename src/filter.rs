466f8af9c5637092bac4d322254c23c33f0b582d
Bruno Parga <brunoparga@gmail.com>
2024-10-02 17:46:56 +0300

    Revert "Pass the client into the filter functions"
    This reverts commit fb0575d96be66db37fa09c721d0d07a1af5a3379.

--- src/filter.rs
+++ src/filter.rs
@@ -2,17 +2,16 @@ use zephyr_sdk::soroban_sdk::xdr::{
     ManageBuyOfferResult, ManageOfferSuccessResultOffer, ManageSellOfferResult, OperationResultTr,
     TransactionResultMeta, TransactionResultResult,
 };
-use zephyr_sdk::EnvClient;
 
 use crate::config::USDC;
 use crate::db::swap::Swap;
 use crate::utils::extract_transaction_results;
 
-pub fn swaps(transaction_results: Vec<TransactionResultMeta>, client: &EnvClient) -> Vec<Swap> {
+pub fn swaps(transaction_results: Vec<TransactionResultMeta>) -> Vec<Swap> {
     transaction_results
         .iter()
         .filter(|transaction_result| is_successful(transaction_result))
-        .filter_map(|result| build_swaps(result, client))
+        .filter_map(build_swaps)
         .flatten()
         .collect::<Vec<Swap>>()
 }
@@ -24,21 +23,15 @@ fn is_successful(result_meta: &TransactionResultMeta) -> bool {
     )
 }
 
-fn build_swaps(
-    transaction_result: &TransactionResultMeta,
-    client: &EnvClient,
-) -> Option<Vec<Swap>> {
+fn build_swaps(transaction_result: &TransactionResultMeta) -> Option<Vec<Swap>> {
     let operation_results = extract_transaction_results(transaction_result);
     // It could be that a transaction does not contain swaps (e.g. a simple payment,
     // an account creation).
-    let potential_swaps: Vec<Swap> = operation_results
-        .iter()
-        .filter_map(|result| build_swap(result, client))
-        .collect();
+    let potential_swaps: Vec<Swap> = operation_results.iter().filter_map(build_swap).collect();
     (!potential_swaps.is_empty()).then_some(potential_swaps)
 }
 
-fn build_swap(operation_result: &OperationResultTr, client: &EnvClient) -> Option<Swap> {
+fn build_swap(operation_result: &OperationResultTr) -> Option<Swap> {
     match operation_result {
         // At the moment, these are the only swaps we can reliably detect.
         // Later we might also get swaps from PathPayments.
