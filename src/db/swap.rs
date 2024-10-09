a8fa27044c0e100c1f9ba6b734078c53531a40ee
Bruno Parga <brunoparga@gmail.com>
2024-10-09 10:34:16 +0300

    Prepare to use issuers to id floating assets

--- src/db/swap.rs
+++ src/db/swap.rs
@@ -15,12 +15,11 @@ use crate::{
 #[with_name("swaps")]
 pub struct SwapDbRow {
     pub creation: u64,
-    pub stable: String,
-    pub stableamt: i64,
-    // This is a stand-in for a boolean: 1 means the swap was a
-    // stablecoin sale, 0 means a purchase
-    pub stbl_sold: i32,
-    pub floating: String,
+    pub usdc_amnt: i64,
+    // This stands in for a bool: 1 means the swap was a USDC sale, 0 = purchase.
+    pub usdc_sale: i32,
+    pub floatcode: String,
+    pub fltissuer: String,
     pub numerator: i64,
     pub denom: i64,
 }
@@ -29,10 +28,10 @@ impl SwapDbRow {
     pub fn new(swap: &Swap, timestamp: u64) -> Self {
         Self {
             creation: timestamp,
-            stable: swap.stablecoin.clone(),
-            stableamt: swap.stablecoin_amount as i64,
-            stbl_sold: swap.is_stablecoin_sale as i32,
-            floating: swap.floating_asset.clone(),
+            usdc_amnt: swap.usdc_amount as i64,
+            usdc_sale: swap.is_usdc_sale as i32,
+            floatcode: swap.floating_asset_code.clone(),
+            fltissuer: swap.floating_asset_issuer.clone(),
             numerator: swap.price_numerator,
             denom: swap.price_denominator,
         }
@@ -42,17 +41,17 @@ impl SwapDbRow {
 #[derive(Clone, Debug)]
 pub struct Swap {
     pub created_at: Option<u64>,
-    pub stablecoin: String,
-    pub stablecoin_amount: f64,
-    pub is_stablecoin_sale: bool,
-    pub floating_asset: String,
+    pub usdc_amount: f64,
+    pub is_usdc_sale: bool,
+    pub floating_asset_code: String,
+    pub floating_asset_issuer: String,
     pub price_numerator: i64,
     pub price_denominator: i64,
 }
 
 impl Display for Swap {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let direction = if self.is_stablecoin_sale {
+        let direction = if self.is_usdc_sale {
             "(sell)"
         } else {
             "(buy)"
@@ -73,10 +72,10 @@ impl Display for Swap {
 
         write!(
             f,
-            "{timestamp} {direction} {} {} for {} at {}",
-            self.stablecoin_amount / CONVERSION_FACTOR,
-            self.stablecoin,
-            self.floating_asset,
+            "{timestamp} {direction} {} USDC for {} ({}) at {}",
+            self.usdc_amount / CONVERSION_FACTOR,
+            self.floating_asset_code,
+            self.floating_asset_issuer,
             self.price_numerator as f64 / self.price_denominator as f64
         )
     }
@@ -87,10 +86,10 @@ impl From<OfferEntry> for Swap {
         if offer_entry.selling == USDC {
             Swap {
                 created_at: None,
-                stablecoin: format_asset(&offer_entry.selling),
-                stablecoin_amount: offer_entry.amount as f64,
-                is_stablecoin_sale: true,
-                floating_asset: format_asset(&offer_entry.buying),
+                usdc_amount: offer_entry.amount as f64,
+                is_usdc_sale: true,
+                floating_asset_code: format_asset(&offer_entry.buying),
+                floating_asset_issuer: "GPLACEHOLDERXYZWQ".to_string(),
                 price_numerator: offer_entry.price.n as i64,
                 price_denominator: offer_entry.price.d as i64,
             }
@@ -99,10 +98,10 @@ impl From<OfferEntry> for Swap {
                 offer_entry.amount as f64 * offer_entry.price.n as f64 / offer_entry.price.d as f64;
             Swap {
                 created_at: None,
-                stablecoin: format_asset(&offer_entry.buying),
-                stablecoin_amount: amount,
-                is_stablecoin_sale: false,
-                floating_asset: format_asset(&offer_entry.selling),
+                usdc_amount: amount,
+                is_usdc_sale: false,
+                floating_asset_code: format_asset(&offer_entry.selling),
+                floating_asset_issuer: "GPLACEHOLDERXYZWQ".to_string(),
                 price_numerator: offer_entry.price.d as i64,
                 price_denominator: offer_entry.price.n as i64,
             }
@@ -116,25 +115,23 @@ impl TryFrom<&ClaimAtom> for Swap {
         let (asset_sold, amount_sold, asset_bought, amount_bought) =
             extract_claim_atom_data(claim_atom);
 
-        let stablecoin = "USDC".to_string();
-
         if *asset_sold == USDC {
             Ok(Swap {
                 created_at: None,
-                stablecoin,
-                stablecoin_amount: amount_sold as f64,
-                is_stablecoin_sale: true,
-                floating_asset: format_asset(asset_bought),
+                usdc_amount: amount_sold as f64,
+                is_usdc_sale: true,
+                floating_asset_code: format_asset(asset_bought),
+                floating_asset_issuer: "GPLACEHOLDERXYZWQ".to_string(),
                 price_numerator: amount_bought,
                 price_denominator: amount_sold,
             })
         } else if *asset_bought == USDC {
             Ok(Swap {
                 created_at: None,
-                stablecoin,
-                stablecoin_amount: amount_bought as f64,
-                is_stablecoin_sale: false,
-                floating_asset: format_asset(asset_sold),
+                usdc_amount: amount_bought as f64,
+                is_usdc_sale: false,
+                floating_asset_code: format_asset(asset_sold),
+                floating_asset_issuer: "GPLACEHOLDERXYZWQ".to_string(),
                 price_numerator: amount_sold,
                 price_denominator: amount_bought,
             })
@@ -148,10 +145,10 @@ impl From<&SwapDbRow> for Swap {
     fn from(row: &SwapDbRow) -> Self {
         Swap {
             created_at: Some(row.creation),
-            stablecoin: row.stable.to_string(),
-            stablecoin_amount: row.stableamt as f64,
-            is_stablecoin_sale: row.stbl_sold == 1,
-            floating_asset: row.floating.to_string(),
+            usdc_amount: row.usdc_amnt as f64,
+            is_usdc_sale: row.usdc_sale == 1,
+            floating_asset_code: row.floatcode.to_string(),
+            floating_asset_issuer: "GPLACEHOLDERXYZWQ".to_string(),
             price_numerator: row.numerator,
             price_denominator: row.denom,
         }
