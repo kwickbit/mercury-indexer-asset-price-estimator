a895c067c1ec0369e62e98eacc06304bdc0dd2ac
Bruno Parga <brunoparga@gmail.com>
2024-10-17 16:45:33 +0300

    Reduce the window to 1 hour, which works

--- src/config.rs
+++ src/config.rs
@@ -14,6 +14,6 @@ pub const CONVERSION_FACTOR: f64 = 10_000_000.0;
 
 // Length of the exchange rate window
 const MINUTE: u64 = 60;
-const HOUR: u64 = 60 * MINUTE;
-const _DAY: u64 = 24 * HOUR;
-pub const RATE_UPDATE_INTERVAL: u64 = 3 * HOUR;
+const _HOUR: u64 = 60 * MINUTE;
+const _DAY: u64 = 24 * _HOUR;
+pub const RATE_UPDATE_INTERVAL: u64 = 60 * MINUTE;
