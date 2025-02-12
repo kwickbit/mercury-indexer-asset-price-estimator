a20c8a926ba495ac493498c4b2725a38b3974402
Bruno Parga <brunoparga@gmail.com>
2025-02-12 16:26:17 +0200

    Make query scripts more universal pt. 2

--- scripts/query/build_query.sh
+++ scripts/query/build_query.sh
@@ -1,44 +1,44 @@
 #! /bin/bash
 
 build_query() {
-    local command="$1"
-    local asset_code="$2"
-    local asset_issuer="$3"
-    local date="$4"
-    local cat_text="$5"
+  local command="$1"
+  local asset_code="$2"
+  local asset_issuer="$3"
+  local date="$4"
+  local cat_text="$5"
 
-    local fname
-    local arguments
+  local fname
+  local arguments
 
-    if [ "$command" = "all" ]; then
-        fname="get_all_exchange_rates"
-        arguments="{}"
-    elif [ "$command" = "asset" ]; then
-        fname="get_exchange_rate"
-        # The way the host server handles arguments requires us to double-escape
-        # the arguments
-        arguments="{\\\"asset_code\\\": \\\"$asset_code\\\""
-        [ -n "$asset_issuer" ] && arguments="$arguments, \\\"asset_issuer\\\": \\\"$asset_issuer\\\""
-        [ -n "$date" ] && arguments="$arguments, \\\"date\\\": \\\"$date\\\""
-        arguments="$arguments}"
-    elif [ "$command" = "cat" ]; then
-        fname="cat"
-        arguments="{\\\"text\\\": \\\"$cat_text\\\"}"
-    elif [ "$command" = "currencies" ]; then
-        fname="get_all_currencies"
-        arguments="{}"
-    elif [ "$command" = "duplicates" ]; then
-        fname="get_duplicate_swaps"
-        arguments="{}"
-    elif [ "$command" = "history" ]; then
-        fname="get_exchange_rate_history"
-        arguments=$(cat "${BASE_DIR}/indexer/scripts/query/history_request.json" | tr -d '\n' | sed 's/"/\\"/g')
-    elif [ "$command" = "savepoint" ]; then
-        fname="savepoint"
-        arguments="{}"
-    elif [ "$command" = "soroswaps" ]; then
-        fname="get_soroswap_swaps"
-        arguments="{}"
-    fi
-    echo "{\"project_name\": \"kwickbit\", \"mode\": {\"Function\": {\"fname\": \"$fname\", \"arguments\": \"$arguments\"}}}"
+  if [ "$command" = "all" ]; then
+    fname="get_all_exchange_rates"
+    arguments="{}"
+  elif [ "$command" = "asset" ]; then
+    fname="get_exchange_rate"
+    # The way the host server handles arguments requires us to double-escape
+    # the arguments
+    arguments="{\\\"asset_code\\\": \\\"$asset_code\\\""
+    [ -n "$asset_issuer" ] && arguments="$arguments, \\\"asset_issuer\\\": \\\"$asset_issuer\\\""
+    [ -n "$date" ] && arguments="$arguments, \\\"date\\\": \\\"$date\\\""
+    arguments="$arguments}"
+  elif [ "$command" = "cat" ]; then
+    fname="cat"
+    arguments="{\\\"text\\\": \\\"$cat_text\\\"}"
+  elif [ "$command" = "currencies" ]; then
+    fname="get_all_currencies"
+    arguments="{}"
+  elif [ "$command" = "duplicates" ]; then
+    fname="get_duplicate_swaps"
+    arguments="{}"
+  elif [ "$command" = "history" ]; then
+    fname="get_exchange_rate_history"
+    arguments=$(cat "${BASE_DIR}/scripts/query/history_request.json" | tr -d '\n' | sed 's/"/\\"/g')
+  elif [ "$command" = "savepoint" ]; then
+    fname="savepoint"
+    arguments="{}"
+  elif [ "$command" = "soroswaps" ]; then
+    fname="get_soroswap_swaps"
+    arguments="{}"
+  fi
+  echo "{\"project_name\": \"kwickbit\", \"mode\": {\"Function\": {\"fname\": \"$fname\", \"arguments\": \"$arguments\"}}}"
 }
